3965fe5cfe24a8d9c732380daaf33d693161bb63
Kare Nuorteva <kare.nuorteva@me.com>
2021-09-28 22:14:18 +0300

    feat: Add subdomains as an option to HSTS

--- hsts_test.go
+++ hsts_test.go
@@ -9,20 +9,34 @@ import (
 )
 
 func TestHSTS(t *testing.T) {
+	type args struct {
+		includeSubDomains bool
+	}
 	tests := []struct {
 		name     string
+		args     args
 		expected string
 	}{
 		{
-			name:     "simple",
+			name: "simple",
+			args: args{
+				includeSubDomains: true,
+			},
 			expected: "max-age=63072000; includeSubDomains",
+		},
+		{
+			name: "simple no sub domains",
+			args: args{
+				includeSubDomains: false,
+			},
+			expected: "max-age=63072000",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			rr := httptest.NewRecorder()
 			req := newRequest(t, http.MethodGet, "/x", "")
-			srv := httpx.HSTS(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+			srv := httpx.HSTS(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}), tt.args.includeSubDomains)
 			srv.ServeHTTP(rr, req)
 			res := rr.Result()
 			if header := res.Header.Get("Strict-Transport-Security"); tt.expected != header {
