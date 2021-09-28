d1bba0e8a48dc14838cd66cfaba45c130c83bc1c
Kare Nuorteva <kare.nuorteva@me.com>
2021-09-28 22:13:05 +0300

    docs: Update Go doc

--- doc.go
+++ doc.go
@@ -1,2 +1,17 @@
-// Package httpx provides useful HTTP server middleware.
+// Package httpx provides a convenience wrapper for http.Server and a middleware
+// wrapper for http.MaxBytesReader.
+//
+// Server example:
+//		options := func(srv *http.Server) {
+//			srv.Handler = ...
+//			srv.ReadTimeout = time.Second * 5
+//		}
+//		srv := httpx.NewServer(":http", options)
+//		go srv.ListenAndServe()
+//		srv.Shutdown()
+//
+// MaxBytesReader example for chi:
+//		r := chi.NewRouter()
+//		r.Use(httpx.MaxBytesReader(1 << 20))
+//
 package httpx
