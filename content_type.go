9ad27e0dc019bbc584436d0112a6320766a28bfe
Kare Nuorteva <kare.nuorteva@me.com>
2021-10-14 19:41:55 +0300

    fix: hide ContentType(Type, Handler) from public API

--- content_type.go
+++ content_type.go
@@ -8,21 +8,21 @@ import (
 
 // ContentTypeJSON sets the response ’Content-Type’ HTTP header with value `application/json;charset=utf-8`.
 func ContentTypeJSON(h http.Handler) http.Handler {
-	return ContentType(contenttype.JSON, h)
+	return contentType(contenttype.JSON, h)
 }
 
 // ContentTypeHTML sets the response ’Content-Type’ HTTP header with value `text/html;charset=utf-8`.
 func ContentTypeHTML(h http.Handler) http.Handler {
-	return ContentType(contenttype.HTML, h)
+	return contentType(contenttype.HTML, h)
 }
 
 // ContentTypeText sets the response ’Content-Type’ HTTP header with value `text/plain;charset=utf-8`.
 func ContentTypeText(h http.Handler) http.Handler {
-	return ContentType(contenttype.Text, h)
+	return contentType(contenttype.Text, h)
 }
 
-// ContentType sets the response `Content-Type` HTTP header with given ct (HTML, JSON, etc).
-func ContentType(ct contenttype.Type, h http.Handler) http.Handler {
+// contentType sets the response `Content-Type` HTTP header with given ct (HTML, JSON, etc).
+func contentType(ct contenttype.Type, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		value := ct.String() + contenttype.CharsetUTF8
 		w.Header().Set(contenttype.Header, value)
