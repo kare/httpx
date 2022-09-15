4588c4db63c38f9cfcb09243e37061181f233bef
Kare Nuorteva <kare.nuorteva@me.com>
2022-09-15 18:19:13 +0300

    docs: update Go doc

--- doc.go
+++ doc.go
@@ -1,17 +1,2 @@
-// Package httpx provides a convenience wrapper for http.Server and a middleware
-// wrapper for http.MaxBytesReader.
-//
-// Server example:
-//		options := func(srv *http.Server) {
-//			srv.Handler = ...
-//			srv.ReadTimeout = time.Second * 5
-//		}
-//		srv := httpx.NewServer(":http", options)
-//		go srv.ListenAndServe()
-//		srv.Shutdown()
-//
-// MaxBytesReader example for chi:
-//		r := chi.NewRouter()
-//		r.Use(httpx.MaxBytesReader(1 << 20))
-//
+// Package httpx provides convinience functions and types for HTTP servers.
 package httpx
